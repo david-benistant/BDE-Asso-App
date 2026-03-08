19eee5db83f4438e8f3ba1a3d80e9923a6213f30
David Benistant <david.benistant@epitech.eu>
2026-03-08 12:47:05 +0100

    update jenkinsfile

--- backend/lambdas/install.sh
+++ backend/lambdas/install.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-export PATH="/home/ubuntu/.bun/bin:$PATH"
-
-if command -v bun >/dev/null 2>&1; then
-  PKG_MANAGER="bun install"
-elif command -v pnpm >/dev/null 2>&1; then
-  PKG_MANAGER="pnpm install"
-elif command -v yarn >/dev/null 2>&1; then
-  PKG_MANAGER="yarn install"
-else
-  PKG_MANAGER="npm install"
-fi
-
-
-echo "Using package manager: $PKG_MANAGER"
-
 for dir in ./*/; do
   if [ -f "$dir/package.json" ]; then
     echo "Installing in $dir"
-    (cd "$dir" && $PKG_MANAGER)
+    (cd "$dir" && bun install)
   fi
 done
